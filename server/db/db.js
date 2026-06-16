7fe7a5bff510994bcf87694ffcdfad6341002c62
mackaylarodriguez <mackaylarodriguez6@gmail.com>
2026-06-16 10:49:27 -0700

    Fix Supabase SSL connection for Render deployment

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // Required for Supabase when connecting from Render (external host)
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
